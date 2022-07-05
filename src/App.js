3266f1e6fea980ec4e31b5ec465c056ded320678
sereja-art <226@tweld.local>
2022-07-05 17:25:15 +0300

    Добавляю верстку проекта

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import face1 from './img/face1.jpg';
+import face2 from './img/face2.jpg';
+import face3 from './img/face3.jpg';
+import userAvatarImg from './img/user-face.jpg';
+import logo from './img/logo_logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className='header'>
         <a href='/' className='logo'>
-          <img src='#' className='logo__img' alt='logo'></img>
+          <img src={logo} className='logo__img' alt='logo'></img>
         </a>
       </header>
-      <main>
-        <section className='sidebar'>
+      <section className='sidebar'>
           <ul className='sidebar__menu'>
             <li className='sidebar__menu-list-item'>
               <a href='/' className='sidebar__menu-link'>Профиль</a>
@@ -28,40 +32,42 @@ function App() {
 
           <a href='/' className='sidebar__settings'>Настройки</a>
 
-          <div className="friends">
-            <div className="friend">
+          <div className="friend">
+            <div className="friend__item">
               <a href="/" className="friend__logo">
-                <img src="" alt="" className="friend__logo-img" />
+                <img src={face1} alt="" className="friend__logo-img" />
               </a>
               <a href="/" className="friend__name">Андрей</a>
             </div>
-            <div className="friend">
+            <div className="friend__item">
               <a href="/" className="friend__logo">
-                <img src="" alt="" className="friend__logo-img" />
+                <img src={face2} alt="" className="friend__logo-img" />
               </a>
               <a href="/" className="friend__name">Саша</a>
             </div>
-            <div className="friend">
+            <div className="friend__item">
               <a href="/" className="friend__logo">
-                <img src="" alt="" className="friend__logo-img" />
+                <img src={face3}alt="" className="friend__logo-img" />
               </a>
               <a href="/" className="friend__name">Света</a>
             </div>
           </div>
         </section>
+      <main className="main-content">
+
         <section className="profile">
-          <img src="/" alt="" className="profile__top-banner" />
+          <div className="profile__top-banner" />
           <div className="user">
             <a href="/" className="user__avatar">
-              <img src="" alt="" className="user__avatar-img" />
+              <img src={userAvatarImg} alt="" className="user__avatar-img" />
             </a>
             <div className="user__info">
-              <p className="user__name">Сергей Артемов</p>
+              <p className="user__name">Иван Петров</p>
               <ul className="user__info-list">
-                <p className="user__born">Дата рождения: 29 октября</p>
+                <p className="user__born">Дата рождения: 12 октября</p>
                 <p className="user__city">Город: Курск</p>
-                <p className="user__education">Образование: ЮЗГУ</p>
-                <p className="user__website">Сайт: sereja-art.ru</p>
+                <p className="user__education">Образование: шарага</p>
+                <p className="user__website">Сайт: нету его</p>
               </ul>
             </div>
           </div>
