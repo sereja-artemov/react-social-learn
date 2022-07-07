0a8ef492bdfe55fa711e8fb982fb671bd5ec73dc
sereja-art <sereja.artemov.art@yandex.ru>
2022-07-07 22:46:12 +0300

    m

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,8 @@
 import React from "react";
 import logo from "../img/logo_logo.svg";
 
-<<<<<<< HEAD
 const Header = () => {
-=======
-function Header() {
->>>>>>> 137a7c777bbeeea48c808f967a4074d68dce769e
+
     return (
         <header className='header'>
             <a href='/' className='logo'>
